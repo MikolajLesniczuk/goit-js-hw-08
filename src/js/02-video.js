45ecbd744e8a4e6153ab1c0a1c5c8d7903b951ed
MikolajLesniczuk <119354331+MikolajLesniczuk@users.noreply.github.com>
2023-04-30 13:25:40 +0200

    finalcommit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
 
+
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+
+
+const timePlay = (data) => {
+  
+const {seconds} = data;
+console.log(`Actually watched : ${seconds}s`);
+localStorage.setItem('videoplayer-current-time',seconds);
+return seconds;
+} 
+
+const newTime = () => {
+const currentTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(currentTime);
+
+}
+
+
+player.on('timeupdate',throttle(timePlay , 1000 ));
+
+iframe.addEventListener('load', newTime )
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,86 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form')
+const input = document.querySelector(`input[name="email"]`)
+const textarea = document.querySelector(`textarea[name="message"]`)
+const STORAGE_KEY = 'feedback-form-state'
+
+
+const handleSubmit = (event) => {
+event.preventDefault() ;
+
+try {
+const inputData = JSON.parse(localStorage.getItem(STORAGE_KEY)) 
+const {
+    email: inputEmail, 
+    message: inputMessage
+    } = inputData;
+
+
+const submitted = {
+email: inputEmail,
+message: inputMessage
+};
+
+inputEmail.email = '';
+inputMessage.message = ''; 
+
+console.log('submitted data:', submitted);
+
+
+localStorage.setItem(STORAGE_KEY, JSON.stringify(inputData));
+
+form.reset();
+localStorage.removeItem(STORAGE_KEY);
+
+}
+
+catch (error) {
+    console.error("Set state error: ", error.message);
+    alert("something wrong")
+}
+
+}
+
+const newValue = (event) => {
+event.preventDefault();
+
+try{
+const {email,message} = form;
+
+const newData = {
+    email: email.value,
+    message: message.value
+};
+
+
+localStorage.setItem(STORAGE_KEY, JSON.stringify(newData))
+} 
+
+catch (error) {
+
+    console.error("Set state error: ", error.message);
+    alert("something wrong")
+}
+
+}
+
+const loadData = () => {
+try {
+    const inputData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || ""
+    input.value = inputData.email || "";
+    textarea.value = inputData.message || ""; }
+catch (error) {
+    console.error("Set state error: ", error.message);
+    alert("something wrong")
+}
+
+}
+
+ 
+    
+
+window.addEventListener('load',loadData);
+form.addEventListener('input',throttle(newValue,500));
+form.addEventListener('submit',handleSubmit);
 
